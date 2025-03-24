d4167ed317aa7752e8c394b6eb3d81093ce108bf
Frank Casanova <frankcasanova.info@gmail.com>
2025-03-25 00:45:58 +0100

    feat(mob): add Mob module with random movement and visibility handling
    Add a new Mob module that implements a CharacterBody3D with random movement and visibility handling. The Mob class includes initialization, physics processing, and methods for handling screen exit events. The rand crate is added as a dependency to support random number generation for movement behavior.

--- rust_godot_boilerplate/src/lib.rs
+++ rust_godot_boilerplate/src/lib.rs
@@ -1,3 +1,4 @@
+mod mob;
 use godot::prelude::*;
 
 mod player;
---
+++ rust_godot_boilerplate/src/mob.rs
@@ -0,0 +1,59 @@
+use godot::prelude::*;
+use godot::classes::CharacterBody3D;
+use godot::classes::ICharacterBody3D;
+use rand::Rng;
+use std::f32::consts::PI;
+
+
+#[derive(GodotClass)]
+#[class(base=CharacterBody3D)]
+pub struct Mob {
+    min_speed: f32,
+    max_speed: f32,
+    base: Base<CharacterBody3D>
+}
+
+#[godot_api]
+impl ICharacterBody3D for Mob {
+    fn init(base: Base<CharacterBody3D>) -> Self {
+        godot_print!("Mob initialized");
+        Self {
+            min_speed: 10.0,
+            max_speed: 18.0, 
+            base
+         }
+    }
+    fn physics_process(&mut self, _delta: f64) {
+        self.base_mut().move_and_slide();
+    }
+    
+}
+#[godot_api]
+impl Mob {
+
+    #[func]
+    fn on_visible_on_screen_notifier_3d_screen_exited(&mut self) {
+        self.base_mut().queue_free();
+    }
+
+    #[func]
+    fn initialize(&mut self, start_position: Vector3, player_position: Vector3) {
+        self.base_mut().look_at_from_position(player_position, start_position);
+        self.base_mut().rotate_y(rand::rng().random_range(-PI/4.0..PI/4.0));
+        let random_speed = rand::rng().random_range(self.min_speed..self.max_speed);
+        self.base_mut().set_velocity(Vector3::FORWARD * random_speed);
+
+        
+        // translation for this code
+        //velocity = velocity.rotated(Vector3.UP, rotation.y)
+        // if we're setting a parameter of the node and
+        // in that set there are another parameters
+        // we need to use base_mut() to get those parameters first
+        // then we can set the parameters
+        //self.base_mut().set_velocity(Vector3::FORWARD * random_speed);
+        let rotation = self.base().get_rotation();
+        let velocity = self.base().get_velocity();
+        self.base_mut().set_velocity(velocity.rotated(Vector3::UP, rotation.y));
+    }
+
+}
